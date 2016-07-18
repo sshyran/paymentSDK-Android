08e599e956b421db5611ac39d3c2129f8b3354e3
PaymentSDK <paymentsdk@wirecard.com>
2016-07-18 13:41:21 +0200

    demo apps updated

--- FullScreenFormDemoApp/app/src/main/java/fullscreenform/de/wirecard/fullscreenformdemoapp/MainActivity.java
+++ FullScreenFormDemoApp/app/src/main/java/fullscreenform/de/wirecard/fullscreenformdemoapp/MainActivity.java
@@ -3,6 +3,8 @@ package fullscreenform.de.wirecard.fullscreenformdemoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
@@ -10,6 +12,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 import java.util.UUID;
@@ -19,21 +22,25 @@ import de.wirecard.paymentsdk.WirecardClientBuilder;
 import de.wirecard.paymentsdk.WirecardEnvironment;
 import de.wirecard.paymentsdk.WirecardException;
 import de.wirecard.paymentsdk.WirecardPaymentResponse;
+import de.wirecard.paymentsdk.WirecardPaymentType;
 import de.wirecard.paymentsdk.WirecardResponseError;
 import de.wirecard.paymentsdk.WirecardResponseListener;
 import de.wirecard.paymentsdk.WirecardTransactionType;
 import de.wirecard.paymentsdk.models.WirecardCardPayment;
+import de.wirecard.paymentsdk.models.WirecardPayPalPayment;
+import de.wirecard.paymentsdk.models.WirecardPayment;
+import de.wirecard.paymentsdk.models.WirecardSepaPayment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private WirecardClient wirecardClient;
+    private TextView resultLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         String environment = WirecardEnvironment.TEST.getValue();
         try {
@@ -43,18 +50,24 @@ public class MainActivity extends AppCompatActivity {
             Log.d(de.wirecard.paymentsdk.BuildConfig.APPLICATION_ID, "device is rooted");
         }
 
-        makeTransaction();
+        resultLabel = (TextView) findViewById(R.id.result);
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        findViewById(R.id.card).setOnClickListener(this);
+        findViewById(R.id.paypal).setOnClickListener(this);
+        findViewById(R.id.sepa).setOnClickListener(this);
     }
 
 
-    private void makeTransaction() {
+    private void makeTransaction(WirecardPaymentType wirecardPaymentType, WirecardTransactionType transactionType) {
 
         // for testing purposes only, do not store your merchant account ID and secret key inside app
         String timestamp = generateTimestamp();
         String merchantID = "";
         String secretKey = "";
         String requestID = UUID.randomUUID().toString();
-        WirecardTransactionType transactionType = WirecardTransactionType.PURCHASE;
         BigDecimal amount = new BigDecimal(10);
         String currency = "EUR";
 
@@ -63,20 +76,39 @@ public class MainActivity extends AppCompatActivity {
 
         String signature = generateSignature(data);
 
-        WirecardCardPayment wirecardCardPayment =
-                new WirecardCardPayment(timestamp, requestID, merchantID,
+        WirecardPayment wirecardPayment = null;
+
+        switch (wirecardPaymentType) {
+            case CREDIT_CARD:
+                wirecardPayment = new WirecardCardPayment(timestamp, requestID, merchantID,
                         transactionType, amount,
                         currency, signature);
+                break;
+            case PAYPAL:
+                wirecardPayment = new WirecardPayPalPayment(timestamp, requestID, merchantID,
+                        transactionType, amount,
+                        currency, signature);
+                break;
+            case SEPA:
+                wirecardPayment = new WirecardSepaPayment(timestamp, requestID, merchantID, transactionType,
+                        amount, currency, signature, "creditorID", "mandateID", new Date(),
+                        "merchantName", null);
+                break;
+        }
 
-        wirecardClient.makePayment(wirecardCardPayment, null, new WirecardResponseListener() {
+        wirecardClient.makePayment(wirecardPayment, null, new WirecardResponseListener() {
             @Override
             public void onSuccess(WirecardPaymentResponse wirecardPaymentResponse) {
-                Log.d(de.wirecard.paymentsdk.BuildConfig.APPLICATION_ID, "success");
+                Log.d(de.wirecard.paymentsdk.BuildConfig.APPLICATION_ID, "response received");
+
+                resultLabel.setText(wirecardPaymentResponse.getTransactionState().getValue());
             }
 
             @Override
             public void onError(WirecardResponseError wirecardResponseError) {
                 Log.d(de.wirecard.paymentsdk.BuildConfig.APPLICATION_ID, wirecardResponseError.getErrorMessage());
+
+                resultLabel.setText(wirecardResponseError.getErrorMessage());
             }
         });
     }
@@ -108,5 +140,20 @@ public class MainActivity extends AppCompatActivity {
             sb.append(Integer.toString((mbs[i] & 0xff) + 0x100, 16).substring(1));
         }
         return sb.toString();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.card:
+                makeTransaction(WirecardPaymentType.CREDIT_CARD, WirecardTransactionType.PURCHASE);
+                break;
+            case R.id.paypal:
+                makeTransaction(WirecardPaymentType.PAYPAL, WirecardTransactionType.DEBIT);
+                break;
+            case R.id.sepa:
+                makeTransaction(WirecardPaymentType.SEPA, WirecardTransactionType.PENDING_DEBIT);
+                break;
+        }
     }
 }
